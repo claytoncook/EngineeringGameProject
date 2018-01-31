8acc40a9faaf673ada0c0d065b76086b8a9f329a
Clayton Cook <claytoncook@Claytons-MacBook-Pro.local>
2018-01-31 14:59:43 -0500

    grasstiles/enviroment init

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 //cache elements
 var $tileContainer = document.querySelector('div#tile-container'),
-    $grassTile = document.querySelector('div#grass-tile'),
     $player = document.querySelector('div#player');
 
-var grassTile = function(x, y) {
+var grassTile = function (x, y) {
     this.x = x;
     this.y = y;
     var newGrassTile = document.createElement('grasstile');
@@ -18,12 +17,12 @@ function createTileMap() {
     for (i = 0; i < 2304; i++) {
         new grassTile(x, y);
         x += 30;
-        if (x > 1920) {
+        if (x > 1890) {
             x = 0;
             y += 30;
         }
         console.log(x, y);
-    }    
+    }
 }
 
 createTileMap();
