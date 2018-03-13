fbf498a2917fdf677c9fd6e340d3382658630270
Clayton Cook <claytoncook@Claytons-MacBook-Pro.local>
2018-03-13 09:23:10 -0400

    tile problem

--- scripts/collide.js
+++ scripts/collide.js
@@ -1,10 +1,8 @@
 var collide = function (x, y) {
     if (0 < x + player.width) {
         player.x = x;
-        console.log(true)
     } else if (window.innerWidth <= x + player.width) {
         player.x = x;
-        console.log(true)
     } else {
         player.x += player.speed;
     }
--- scripts/gen-world.js
+++ scripts/gen-world.js
@@ -22,29 +22,35 @@ function createTileMap() {
     var player = 1,
         enemies = 100;
 
-    for (i = 0; i < 2304; i++) {
+    //spawn tiles
+    //spawns tiles in rows
+    for (i = 0; i <= (parseInt($game.style.height)/30); i++) {
         //holds a string, which refers back to a tile type, that is then used throughout this whole function
         var randomTileHolder = randomTile();
+        //random number so the enemies don't spawn by each other
         var random = Math.random();
         //make new tile using constructor function
         //uses the randomTile() function to determine what kind of tile will be placed
         new tile(randomTileHolder, x, y);
         if (randomTileHolder === "grasstile" && enemies > 0 && random < 0.01) {
+            //spawn enemies on a grasstile
             new enemy(randomEnemy(), x, y)
             enemies -= 1;
-        } 
-        if (randomTileHolder === "grasstile" && player > 0 && random < 0.02) {
-            spawnPlayer(x, y)
+        } else if (randomTileHolder === "grasstile" && player > 0 && random < 0.02) {
+            //spawn player on a grasstile
+            spawnPlayer(1080, 0)
             player -= 1;
         }
+        //make next tile next to last generated one
         x += tileWidth;
-        if (x > 1890) {
+        if (x > (parseInt($game.style.width))) {
             //if the a tile's x goes off the screen
             //make x zero, moving it all the way back to the right of the screen
             x = 0;
             //add a tile's height to y so that it wraps over and over
             y += tileHeight;
         }
-
+        randomTileHolder = undefined;
+        console.log(true)
     }
 }
